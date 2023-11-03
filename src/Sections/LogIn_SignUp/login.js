0e9033e7b374d07a50b505d3ecb5ddba17b775fb
Vishal Ola <vishalola555@gmail.com>
2023-11-03 23:50:55 +0530

    login and sign up section added

---
+++ src/Sections/LogIn_SignUp/login.js
@@ -0,0 +1,89 @@
+import TextField from '@mui/material/TextField'
+import LoadingButton from '@mui/lab/LoadingButton';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+export default function Login(props){
+
+    const [passCheck,setPassCheck]=useState(true);
+    const [userCheck,setUserCheck]=useState(true);
+    const [load,setLoading]=useState(false);
+    const navigate=useNavigate();
+    // function setLoggedInCookie() {
+    //     // setting up cookie in browser for 30 dates from time of log in 
+    //     const expiryDate = new Date();
+    //     expiryDate.setDate(expiryDate.getDate() + 30);
+    //     document.cookie = `isLoggedIn=true; expires=${expiryDate.toUTCString()}; path=/`;
+    //   }
+    // let handleClick=(e)=>{
+    //     setLoading(true);
+    //     e.preventDefault();
+    //     let username=e.target[0].value
+    //     let password=e.target[2].value
+        
+    //     // verifying from server if user exists: 
+    //     axios.post("https://findmybuddy-backend.onrender.com/login",{
+    //         "username":username,
+    //         "password":password
+    //     }).then(res=>{
+    //         setLoading(false);
+    //         let data=res.data;
+    //         if(data.found)
+    //         {
+    //             if(data.match)
+    //             {
+    //                 //User data and  Password Match ,  then get user details and redirect to profile page.
+    //                 axios.post("https://findmybuddy-backend.onrender.com/userDetail",{
+    //                     "username":username
+    //                 }).then(res=>{
+    //                     let details=res.data;
+    //                     document.cookie = `UserName=${encodeURIComponent(details.userName)}; path=/`;
+    //                     props.setName(details.Name);
+    //                     props.setEmail(details.Email)
+    //                     props.setNumber(details.Number);
+    //                     props.setUserName(details.userName);
+    //                     props.setLog(true);
+    //                     setLoggedInCookie();
+    //                     navigate("/profile");
+    //                 })
+                    
+    //             }
+    //             else
+    //             {
+    //                 // Password didn't match
+    //                 setPassCheck(false);
+    //             }
+    //         }
+    //         else
+    //         {
+    //             // User not found
+    //             setUserCheck(false)
+    //         }
+    //     })
+    // }
+    return (
+        <div className=" h-[100vh] flex justify-center items-center">
+            
+            <div className="outlin bg-white shadow-2xl  border-b-4 border-b-blue-700  w-[400px]">
+                <div className="
+                p-3 text-4xl
+                outlin flex mt-8 mb-4  text-blue-700  items-center justify-center">
+                    Log In
+                </div>
+                <form  
+                // onSubmit={handleClick}
+                 className="outlin mx-8 p-2 flex justify-center px-4 flex-col gap-4  mb-12">
+                    <TextField required  error={!userCheck} onChange={()=>setUserCheck(true)} label="Username" variant="outlined" />
+                    <TextField required onChange={()=>setPassCheck(true)} error={!passCheck} type="password" label="Password" variant="outlined" />
+                    <LoadingButton loading={load}  className={`${!load?'!bg-blue-700':''}`} type='submit' variant='contained'>Log In</LoadingButton>
+                    <div 
+                    onClick={()=>navigate("/signup")}
+                     className='text-sm outlin text-right cursor-pointer hover:underline'>
+                    ...sign up instead?
+                    </div>
+                </form>
+                
+            </div>
+        
+        </div>
+    )
+}
---
+++ src/Sections/LogIn_SignUp/signup.js
@@ -0,0 +1,79 @@
+import TextField from '@mui/material/TextField'
+import LoadingButton from '@mui/lab/LoadingButton';
+
+import { useState } from 'react'
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+
+export default function SignUp(){
+    const [passCheck,setPassCheck]=useState(false);
+    const [userCheck,setUserCheck]=useState(false);
+    const [load,setLoading]=useState(false);
+    // const navigate=useNavigate();
+    // let handleClick=(e)=>{
+    //     setLoading(true);
+    //     e.preventDefault();
+    //     let username=e.target[0].value
+    //     let name=e.target[2].value
+    //     let email=e.target[4].value
+    //     let number=e.target[6].value
+    //     let password=e.target[8].value
+    //     let confirmPass=e.target[10].value
+    //     if(password!==confirmPass)
+    //     {
+    //         // password didn't match with confirmPassword;
+    //         setPassCheck(true)
+    //         setLoading(false);
+    //     }
+    //     else
+    //     {
+    //         // password matched
+    //         setPassCheck(false);
+    //         axios.post("https://findmybuddy-backend.onrender.com/addUser",{
+    //             "name":name,
+    //             "username":username,
+    //             "email":email,
+    //             "number":number,
+    //             "password":password
+    //         }).then(res=>{
+    //             setLoading(false);
+    //             let check=res.data;
+    //             if(check)
+    //             {
+    //                 // user credentials added to database.
+    //                 navigate("/login")
+    //             }
+    //             else
+    //             {
+    //                 // User already exists
+    //                 setUserCheck(true);
+    //             }
+    //         })
+    //     }
+
+    // }
+    return (
+        <div className="h-[100vh] flex justify-center items-center ">
+            
+            <div className="outlin shadow-2xl bg-white  border-b-4 border-b-blue-700  w-[380px]">
+                <div className="
+                p-3 text-4xl
+                md:text-3xl
+                outlin flex mt-4 mb-4 text-blue-700  items-center justify-center">
+                    Sign Up
+                </div>
+                <form 
+                // onSubmit={handleClick}
+                className="outlin mx-8 p-2 flex justify-center px-4 flex-col gap-4 mb-12 ">
+                    <TextField required error={userCheck} onChange={()=>setUserCheck(false)} label="Username" variant="outlined" />
+                    <TextField required  label="Full Name" variant="outlined" />
+                    <TextField required type="email" label="Email" variant="outlined" />
+                    <TextField required  type="password" label="Password" variant="outlined" />
+                    <TextField onChange={()=>setPassCheck(false)} required error={passCheck}  type="password" label="Confirm Password" variant="outlined" />
+                    <LoadingButton loading={load} type='submit' className={`${!load?'!bg-blue-700':''}`} variant='contained'>Sign Up</LoadingButton>
+                </form>
+            </div>
+        
+        </div>
+    )
+}
