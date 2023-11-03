c3cded0df47d4ae598575afe669ac91d1baf6d40
Vishal Ola <vishalola555@gmail.com>
2023-11-03 23:41:24 +0530

    Custom

---
+++ src/Sections/AddIcon/index.js
@@ -0,0 +1,8 @@
+export default function AddIcon(){
+    return (
+        <div className="relative cursor-pointer  max-h-[50px] max-w-[50px]  h-full w-full rounded-[100%] shadow-[0_0_3px_gray] flex items-center justify-center">
+            <div className="w-[60%] border-2 rounded-lg border-blue-700 rotate-90"></div>
+            <div className="absolute rounded-lg w-[60%] border-2 border-blue-700"></div>
+        </div>
+    )
+}
