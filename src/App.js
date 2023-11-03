a96799d84519d2529634f0e817c6664af7b18142
Vishal Ola <vishalola555@gmail.com>
2023-11-03 23:55:35 +0530

    all routes assigned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
+import {Route,Routes} from 'react-router-dom'
+import Login from "./Sections/LogIn_SignUp/login";
+import SignUp from "./Sections/LogIn_SignUp/signup";
+import ProjectPage from "./Sections/Project Page";
+import Slider from "./Sections/misc/slider";
+import Tasks from "./Sections/Tasks";
+import Home from "./Sections/Home";
+import Profile from "./Sections/Profile";
+import CustomAutoComplete from "./Node/Tasks_v2/customAuto";
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Slider isLoggedIn={true}/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/tasks" element={<Tasks/>}/>
+      <Route path="/:pid" element={<ProjectPage/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path = "/signup" element={<SignUp/>}/>
+    </Routes>
+
+    </>
   );
 }
 
